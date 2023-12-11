eca11ce7df3a94f55acc3a85b2762ae08ae476ac
victormg2 <153522213+victormg2@users.noreply.github.com>
2023-12-11 16:54:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <header>
-        <h1>victor miguel garcia garcia</h1>
+        <h1>Victor miguel garcia garcia</h1>
         <h2>Código de color: 0: morado 1:black 2:celeste 3:azul 4:verde 5:amarillo 6:rojo</h2>
     </header>
     <main>
